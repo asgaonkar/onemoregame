d3d77ced7e6b82d797965734cb0782c2a28f5721
Atit Gaonkar <atit.sgaonkar@gmail.com>
2026-09-13 14:53:09 -0500

    Add Count perception game

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './lib/theme'
 import { Home } from './pages/Home'
 import { CenterGame } from './pages/games/CenterGame'
+import { CountGame } from './pages/games/CountGame'
 import { NotFound } from './pages/NotFound'
 
 export function App() {
@@ -11,6 +12,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/center" element={<CenterGame />} />
+          <Route path="/count" element={<CountGame />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
